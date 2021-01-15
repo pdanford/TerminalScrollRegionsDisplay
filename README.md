dfc00eac0580fc4300776d53bb8ce7703bc49498
Peter <pdanford@pdanford.com>
2021-01-15 11:02:44 -0800

    fix scroll bug edge case and refactor
    - Add \r to lines printed to terminal. This:
      1. Fixes an edge case when a single line of a multi-line scroll region is
         visible *and* is the first line printed on new region. When this happens,
         the title line would be incorrectly scrolled up also and disappear.
      2. moves cursor to leftmost column as marker for last line added to region
    - remove leftmost column ↓ "more below" message implementation (not great)
    - Simplified __CheckScreenRefreshTrigger(). Before tried to separate individual
      region reprint from whole screen reprint when terminal window height changed.
      This worked in most cases, but not always due to differences in terminal app
      behavior. Now always does a reprint of all terminal regions when there's any
      change in terminal window dimensions.
    - edit comments and function parameter descriptions for clarity
    - edit README.md for clarity

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Terminal Window Scroll Region(s)
 --------------------------------------------------------------------------------
-This is a python class to manage output to a terminal window in the form of one or more scroll regions.
+This is a python 3.6+ class to manage output to a terminal window in the form of one or more scroll regions.
 
-Each new instance will establish and print lines in a terminal scroll region whose position starts immediately after any previously instantiated scroll region. That is, each region location is based on the number of rows in the region and how many regions have already been created.
+Each new instance will establish and print lines in a terminal scroll region whose position starts immediately after any previously instantiated scroll regions. That is, each region location is based on the number of rows in the region and how many regions have already been created.
 
 pdanford - January 2021
 
@@ -21,9 +21,7 @@ Region width is technically all the way across the terminal window. But visually
 Note that by default there is a small delay after each line is added to allow some readability to regions being updated very fast. This can be set to zero in the AddLine() call if undesirable. Conversely, don't set too large because it's blocking.
 
 ##### Regarding Terminal Window Size
-There is no terminal window size change callback, so instead any scroll region refresh happens when a new line is added through AddLine().
-
-If the terminal window height is not enough to display scroll regions for all scroll region instances, a highlighted ↓ will appear in the first column which means "more scroll regions hidden below". When window height is increased, these are cleared/updated during the next AddLine() call.
+There is no terminal window size change callback, so instead any scroll region refreshes happen when a new line is added through AddLine().
 
 ### Use
 ```
--- ScrollRegion.py
+++ ScrollRegion.py
@@ -16,7 +16,7 @@ class ScrollRegion:
     Note: requires VT100 escape compatibility
     """
 
-    ## ----- initial class values -----
+    ## ----- class values -----
 
     # where 1st instance scroll region is located in terminal window
     __scroll_region_start_row = 1
@@ -27,47 +27,48 @@ class ScrollRegion:
     # a list of all ScrollRegion instances
     __scroll_regions_list = []
 
-    # class global flag at that scroll region(s) didn't have enough room
-    # on terminal screen for their height to display all lines
-    __more_below_message = ""
-
     ## ----- instance variables -----
 
     # storage for scroll region title line 
-    # note this should only be updated through SetTitle() because it modifies
-    # the scroll region start location
+    # note this should only be updated after creation through SetTitle()
+    # because it modifies the scroll region start location
+    # (takes up 1 row of scroll_region_height)
     __title = ""
 
     # storage for last row number of terminal scroll region
     __scroll_region_end_row = 0
 
-    # keeps track of the last row printed to this scroll region
-    __last_printed_row = 0
-
     # small cache for the lines added to the region
     __line_cache = deque()
- 
+
 
     def __init__(self,
                  title = "",
                  scroll_region_height = 8):
         """
+                       title - if not "", takes up 1 row of scroll_region_height
         scroll_region_height - total height of region (including any title line)
         """
-        if scroll_region_height < 1:
-            raise ValueError("ScrollRegion: scroll_region_height must be >= 1")
+        if scroll_region_height < 2 and title != "":
+            raise ValueError\
+              ("ScrollRegion: with title, scroll_region_height must be >= 2")
+            # technically, VT100 compatibility requires at a scroll height of
+            # at least 2 lines, but modern terminal emulators seem to be fine
+            # with 1 (which might be useful to some)
+        elif scroll_region_height < 1:
+            raise ValueError\
+              ("ScrollRegion: without title, scroll_region_height must be >= 1")
             # technically, VT100 compatibility requires at a scroll height of
             # at least 2 lines, but modern terminal emulators seem to be fine
             # with 1 (which might be useful to some)
 
         # instance member initializations
-        self.__line_cache = deque() 
         self.__scroll_region_height = scroll_region_height
         self.__scroll_region_start_row = ScrollRegion.__scroll_region_start_row
         self.__scroll_region_end_row   = (self.__scroll_region_start_row
-                                        + scroll_region_height
-                                        - 1)
-        self.__last_printed_row = self.__scroll_region_end_row
+                                          + scroll_region_height
+                                          - 1)
+        self.__line_cache = deque()
 
         # update class global for next new ScrollRegion instantiated
         ScrollRegion.__scroll_region_start_row = self.__scroll_region_end_row + 1
@@ -78,7 +79,7 @@ class ScrollRegion:
             # interfere with display when terminal window is resized or
             # scrolled back:
             print("\x1bc", end="")   # clear screen
-            print("\x1b[3J", end="") # erase scroll-back buffer (Linux)
+            print("\x1b[3J", end="") # erase terminal scroll-back buffer
 
         ScrollRegion.__scroll_regions_list.append(self)
 
@@ -95,6 +96,19 @@ class ScrollRegion:
             columns, rows = get_terminal_size()
             print(f"\x1b[{rows};1H", end="") # position cursor to window bottom
             print("\n-- done --")
+
+
+    def ClearScrollRegion(self, blanking_string = ""):
+        """
+        Blanks the entire scroll region and line cache (including title
+        area) with blanking_string.
+        """
+        # remove any title so entire scroll region is cleared
+        self.SetTitle("")
+
+        # flush line cache
+        for i in range(self.__scroll_region_height):
+            self.AddLine(blanking_string, 0)
 
 
     def SetTitle(self, title):
@@ -123,9 +137,9 @@ class ScrollRegion:
             terminal_columns, terminal_rows = get_terminal_size()
             if (title_row <= terminal_rows and
                title_row > 0):
-                print(f"\x1b[{title_row};1H", end="")
-                print(self.__title, end="")
-                print("\x1b[K", end="") # clear rest of line
+                print(f"\x1b[{title_row};1H", end="") # position to title row
+                print(f"{self.__title}", end="")      # print title
+                print("\x1b[K", end="")               # clear rest of line
 
 
     def AddLine(self, line, scroll_delay_s = 0.125):
@@ -141,43 +155,19 @@ class ScrollRegion:
             # keep __line_cache length the same as the scroll region height
             self.__line_cache.popleft()
 
-        # Print newly added line to this scroll region according 3 cases:
+        # Print newly added line to this scroll region end row
+        self.__Print(line, scroll_delay_s)
+
         trigger = self.__CheckScreenRefreshTrigger()
         if trigger == "REPRINT_ALL_SCROLL_REGIONS":
-            # 1. entire terminal window needs updating
-            print("\x1b[3J", end="") # erase scroll-back buffer (Linux)
+            # entire terminal window needs updating
 
-            # clear any previous class global flag that there was not
-            # enough room to print region
-            ScrollRegion.__more_below_message = ""
+            # erase terminal scroll-back buffer
+            print("\x1b[3J", end="")
 
             # reprint all regions' cache
             for r in reversed(ScrollRegion.__scroll_regions_list):
                 r.__ReprintScrollRegion()
-        elif trigger == "REPRINT_SCROLL_REGION":
-            # 2. this whole scroll region (only) needs updating
-            print("\x1b[3J", end="") # erase scroll-back buffer (Linux)
-
-            # clear any previous class global flag that there was not
-            # enough room to print region
-            ScrollRegion.__more_below_message = ""
-
-            self.__ReprintScrollRegion()
-        else:
-            # 3. print/add this single line (only) at this scroll region end
-            self.__Print(line, scroll_delay_s)
-
-
-    def ClearScrollRegion(self, blanking_string = ""):
-        """
-        Blanks the entire scroll region and line cache (including title
-        area) with blanking_string.
-        """
-        # remove any title so entire scroll region is cleared
-        self.SetTitle("")
-
-        for i in range(self.__scroll_region_height):
-            self.AddLine(blanking_string, 0)
 
 
     def __Print(self, line, scroll_delay_s):
@@ -185,37 +175,43 @@ class ScrollRegion:
         Internal function to print line string  at the last line of this
         instance's scroll region in the terminal window and scroll up one line.
 
+                  line - string to print at bottom of this scroll region
         scroll_delay_s - scroll delay each line print for readability
         """
         # make sure that this scroll region start is actually on screen
         terminal_columns, terminal_rows = get_terminal_size()
 
         # don't print line if none of scroll region is within terminal window
-        if self.__scroll_region_start_row > terminal_rows:            
-            self.__last_printed_row = 0
-            # set class global flag there was not enough room to print region
-            ScrollRegion.__more_below_message = "\x1b[0;43m↓\x1b[0m"
+        if self.__scroll_region_start_row > terminal_rows:
+            # there was no room to print in this region
             return
 
         # ensure area where line data is to be printed is in terminal window
-        if self.__scroll_region_end_row <= terminal_rows:
-            self.__last_printed_row = self.__scroll_region_end_row
+        if self.__scroll_region_end_row > terminal_rows:
+            # limit if there's only enough room for partial scroll region
+            # (scroll region is truncated) but enough to print line
+            print_row_num = terminal_rows
         else:
-            # limit if there's not enough room (scroll region is truncated)
-            self.__last_printed_row = terminal_rows
-            # set class global flag there was not enough room to print region
-            ScrollRegion.__more_below_message = "\x1b[0;43m↓\x1b[0m"
+            print_row_num = self.__scroll_region_end_row
 
         # set scrolling region for this ScrollRegion instance
-        print(f"\x1b[{self.__scroll_region_start_row};{self.__last_printed_row}r", end="")
+        print(f"\x1b[{self.__scroll_region_start_row};{print_row_num}r", end="")
         # position to end of scroll region before printing line data
-        print(f"\x1b[{self.__last_printed_row};1H", end="")
+        print(f"\x1b[{print_row_num};1H", end="")
 
-        # prepend a newline to cause previous line to scroll up
-        print(f"\n{ScrollRegion.__more_below_message}{line}", end="")
+        if print_row_num - self.__scroll_region_start_row >= 1:
+            # prepend a newline to cause previous lines to scroll up in region
+            print(f"\n{line}\r", end="")
+        else:
+            # don't do the prepend \n for scroll regions of 1 row because some
+            # terminals will scroll any title line too in certain edge cases
+            print(f"{line}\r", end="")
+
 
         # scroll delay each line for readability
         sleep(scroll_delay_s)
+
+        return
 
 
     def __ReprintScrollRegion(self):
@@ -235,55 +231,43 @@ class ScrollRegion:
         """
         Internal function to check if a window resize event happened that
         results in the currently displayed scroll regions being corrupted.
-        Some resizes change where the scroll regions are displayed on the
+        Some resize changes result in the scroll regions being displayed on
         new window coordinates which don't match previously displayed scroll
-        region location because of scroll back. 
+        region location because of scroll-back.
 
-        Return flag strings:
+        If so, indicate a terminal window buffer reset and reprint of all
+        regions' cache is necessary so scroll region coordinates are correct
+        for this new terminal window size
+
+        return flag strings:
+
+                      empty string - no reprint necessary
         REPRINT_ALL_SCROLL_REGIONS - window height has changed in such a way
-                                     that its scroll back buffer should be
+                                     that its scroll-back buffer should be
                                      cleared and all scroll regions should be
                                      reprinted entirely to fix coordinates
-        REPRINT_SCROLL_REGION - window height has increased over this scroll
-                                region by 1 or more lines since last print
-                                which means (only) this scroll region should
-                                be reprinted entirely to fill in the gap rows
-                                (also true for a region that was previously
-                                entirely off screen)
         """
-        # note previous terminal window dimensions
-        prev_terminal_rows = ScrollRegion.__prev_terminal_rows
         # get current terminal window dimensions
         terminal_columns, terminal_rows = get_terminal_size()
-        # update for next trigger check
-        ScrollRegion.__prev_terminal_columns, ScrollRegion.__prev_terminal_rows =\
-                                                 terminal_columns, terminal_rows
 
-        if (terminal_rows < prev_terminal_rows and
-            terminal_rows < ScrollRegion.__scroll_regions_list[
-            len(ScrollRegion.__scroll_regions_list) -1]
-            .__scroll_region_end_row):
-            # terminal_rows shrank and is less than the last scroll region
-            # which causes the terminal's scroll back buffer to activate
+        # assume a scroll region reprint trigger did not happen
+        return_flag = ""
+
+        if terminal_rows != ScrollRegion.__prev_terminal_rows:
+            # terminal_rows changed which may cause the terminal's scroll-back
+            # buffer to activate which causes the scroll region coordinates to
+            # change
+            return_flag = "REPRINT_ALL_SCROLL_REGIONS"
+
+        elif terminal_columns != ScrollRegion.__prev_terminal_columns:
+            # terminal_columns changed which may cause the terminal's
+            # scroll-back buffer to activate because of line wrapping
             # which causes the scroll region coordinates to change
+            return_flag = "REPRINT_ALL_SCROLL_REGIONS"
 
-            # indicate a terminal window reset and reprint of all regions'
-            # cache is necessary so scroll region coordinates are correct for
-            # this new terminal window size
-            return "REPRINT_ALL_SCROLL_REGIONS"
-        elif (self.__last_printed_row < self.__scroll_region_end_row and
-              terminal_rows >= self.__scroll_region_start_row and
-              terminal_rows > self.__last_printed_row):
-            # the last time this scroll region was printed, it was within the
-            # terminal window but didn't have enough room for the entire
-            # region (i.e. it was truncated) or it was entirely off screen
+        # update for next trigger check
+        ScrollRegion.__prev_terminal_columns,\
+             ScrollRegion.__prev_terminal_rows = terminal_columns, terminal_rows
 
-            # but the terminal window height has increased rows since last
-            # print, so the whole scroll region should be reprinted
-            # to fill the gaps
-            return "REPRINT_SCROLL_REGION"
-        else:
-            # indicate a scroll region reprint trigger did not happen
-            return ""
-
+        return return_flag
 
